a7259b4c7a55a5aa13320d9dda85e20c04936d41
Sazzad Hossen <86873483+hossentahir@users.noreply.github.com>
2024-10-30 01:50:24 +0600

    assignment 1 review, loop and object

---
+++ class 3/script.js
@@ -0,0 +1,28 @@
+// let x = prompt("Enter a number");
+
+// if(x % 2 == 0) console.log("even");
+// else console.log("Odd");
+
+// for(let i = 1; i < 100; i+=3){
+
+//     document.write("Hello  " + i);
+     
+// }
+
+// console.log("This is sazzad");
+// console.log("This is sazzad");
+// console.log("This is sazzad");
+// console.log("This is sazzad");
+
+
+let obj = {
+    objName : "Sazzad",
+    objId : "221-35-840",
+    qty : 10
+}
+
+document.write(obj.objName);
+document.write(obj.objId);
+document.write(obj.qty);
+
+
